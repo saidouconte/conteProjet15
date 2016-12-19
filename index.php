70a1cb77657e75b5965d574ec8fab4ce073ce682
Saidou <Saidou@PC-CONTE>
2016-12-19 19:15:50 +0100

    Premier commite du projet cours15

---
+++ index.php
@@ -0,0 +1,18 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Projet gitHub cours 15</h1>
+        <p>Cours du 19-12-2016</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
